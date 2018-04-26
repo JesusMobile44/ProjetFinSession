6a11ac28c00d2574d444aad315f477711c0814a6
Simon Belley <belleysimon44@gmail.com>
2018-04-26 14:52:44 -0400

    Merged

---
+++ planetes/Planete.java
@@ -0,0 +1,24 @@
+package planetes;
+
+/**
+ * Created by BelSi1731422 on 2018-04-26.
+ */
+public class Planete{
+    protected String nom;
+    protected int nbPlutonium;
+    protected int nbNeptunium;
+    protected int nbTerbium;
+    protected int nbganolinium;
+    protected int nbThulium;
+
+
+    public String getNom() {
+        return nom;
+    }
+
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
